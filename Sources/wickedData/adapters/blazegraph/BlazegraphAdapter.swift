b728fd700489a898016b9b27c8d669a58fd076da
Dmitrii <zeionara@niuitmo.ru>
2021-11-07 19:12:23 +0300

    Added support for custom query timeout

--- Sources/wickedData/adapters/blazegraph/BlazegraphAdapter.swift
+++ Sources/wickedData/adapters/blazegraph/BlazegraphAdapter.swift
@@ -11,6 +11,10 @@ public struct BlazegraphAdapter: GraphServiceAdapter {
     }
 
     public func sample<QueryType: Query>(_ query: QueryType) async throws -> Sample<QueryType.BindingType> {
+        try await sample(query, timeout: nil)
+    }
+
+    public func sample<QueryType: Query>(_ query: QueryType, timeout: Int?) async throws -> Sample<QueryType.BindingType> {
         let stringifiedUrl = "\(url)/blazegraph/namespace/kb/sparql"
         guard let url = URL(string: stringifiedUrl) else {
             throw GraphServiceAdapterError.invalidUrl(stringifiedUrl)
@@ -21,6 +25,9 @@ public struct BlazegraphAdapter: GraphServiceAdapter {
 
         urlRequest.setValue("application/sparql-results+json", forHTTPHeaderField: "accept")
         urlRequest.setValue("application/x-www-form-urlencoded; charset=UTF-8", forHTTPHeaderField: "content-type")
+        if let unwrappedTimeout = timeout {
+            urlRequest.setValue(String(describing: unwrappedTimeout), forHTTPHeaderField: "X-BIGDATA-MAX-QUERY-MILLIS")
+        }
 
         urlRequest.httpBody = "query=\(query.text.addingPercentEncoding(withAllowedCharacters: .alphanumerics)!)".data(using: .utf8)!
         // print(query.text.addingPercentEncoding(withAllowedCharacters: .alphanumerics)!)
@@ -46,7 +53,7 @@ public struct BlazegraphAdapter: GraphServiceAdapter {
         return decoded!
     }
 
-    public func update(_ query: UpdateQuery) async throws -> UpdateQuery.BindingType {
+    public func update(_ query: UpdateQuery, timeout: Int? = nil) async throws -> UpdateQuery.BindingType {
         let stringifiedUrl = "\(url)/blazegraph/namespace/kb/sparql"
         guard let url = URL(string: stringifiedUrl) else {
             throw GraphServiceAdapterError.invalidUrl(stringifiedUrl)
@@ -56,6 +63,9 @@ public struct BlazegraphAdapter: GraphServiceAdapter {
         urlRequest.httpMethod = "POST"
 
         urlRequest.setValue("application/x-turtle", forHTTPHeaderField: "content-type")
+        if let unwrappedTimeout = timeout {
+            urlRequest.setValue(String(describing: unwrappedTimeout), forHTTPHeaderField: "X-BIGDATA-MAX-QUERY-MILLIS")
+        }
 
         urlRequest.httpBody = query.text.data(using: .utf8)!
 
